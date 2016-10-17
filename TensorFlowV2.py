ec073b1f831a6bc97fcb3824f068d842b9a578ea
tobinjo <tobinjo@umich.edu>
2016-10-17 09:55:51 -0400

    Add files via upload

---
+++ TensorFlowV2.py
@@ -0,0 +1,50 @@
+from openpyxl import load_workbook
+wb2 = load_workbook('CramerPicks.xlsx')
+wb2.active = 7
+ws = wb2.active
+biglist = []
+jimschoices = []
+rowval = 2
+
+for row in ws.iter_rows('D2:R2150'):
+    rowlist = []
+    good = 1
+    zeros = [0, 0, 0, 0, 0]
+    choice = ws.cell(row = rowval, column = 3)
+    zeros[choice.value-1] = 1
+    for cell in row:
+        if cell.value == '#N/A':
+            good = 0
+        rowlist.append(cell.value)
+    if good:
+        biglist.append(rowlist)
+        jimschoices.append(zeros)
+    rowval += 1
+
+import numpy as np
+import tensorflow as tf
+Xtr = np.asanyarray(biglist[1:2000])
+Ytr = np.asanyarray(jimschoices[1:2000])
+Xte = np.asanyarray(biglist[2001:])
+Yte = np.asanyarray(jimschoices[2001:])
+
+x = tf.placeholder(tf.float32, [None, 15])
+W = tf.Variable(tf.zeros([15, 5]))
+b = tf.Variable(tf.zeros([5]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+y_ = tf.placeholder(tf.float32, [None, 5])
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+
+for i in range(200):
+    batchxs = np.asanyarray(biglist[(i-1)*10:(i*10)-1])
+    batchys = np.asanyarray(jimschoices[(i-1)*10:(i*10)-1])
+    sess.run(train_step, feed_dict={x: batchxs, y_: batchys})
+
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: Xte, y_: Yte}))
---
+++ TensorFlowV3.py
@@ -0,0 +1,106 @@
+from openpyxl import load_workbook
+wb2 = load_workbook('CramerPicks.xlsx')
+wb2.active = 7
+ws = wb2.active
+biglist = []
+jimschoices = []
+rowval = 2
+
+for row in ws.iter_rows('D2:R2150'):
+    rowlist = []
+    good = 1
+    zeros = [0, 0, 0, 0, 0]
+    choice = ws.cell(row = rowval, column = 3)
+    zeros[choice.value-1] = 1
+    for cell in row:
+        if cell.value == '#N/A':
+            good = 0
+        rowlist.append(cell.value)
+    if good:
+        biglist.append(rowlist)
+        jimschoices.append(zeros)
+    rowval += 1
+
+import numpy as np
+import tensorflow as tf
+Xtr = np.asanyarray(biglist[0:1999])
+Ytr = np.asanyarray(jimschoices[0:1999])
+Xte = np.asanyarray(biglist[2000:])
+Yte = np.asanyarray(jimschoices[2000:])
+
+# Parameters
+learning_rate = 0.01
+training_epochs = 30
+batch_size = 100
+display_step = 1
+
+# Network Parameters
+n_hidden_1 = 32 # 1st layer number of features
+n_hidden_2 = 16 # 2nd layer number of features
+n_input = 15
+n_classes = 5
+
+# tf Graph input
+x = tf.placeholder("float", [None, n_input])
+y = tf.placeholder("float", [None, n_classes])
+
+# Create model
+def multilayer_perceptron(x, weights, biases):
+    # Hidden layer with RELU activation
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1 = tf.nn.relu(layer_1)
+    # Hidden layer with RELU activation
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    layer_2 = tf.nn.relu(layer_2)
+    # Output layer with linear activation
+    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    return out_layer
+
+# Store layers weight & bias
+weights = {
+    'h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_hidden_2, n_classes]))
+}
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'out': tf.Variable(tf.random_normal([n_classes]))
+}
+
+# Construct model
+pred = multilayer_perceptron(x, weights, biases)
+
+# Define loss and optimizer
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+init = tf.initialize_all_variables()
+
+with tf.Session() as sess:
+    sess.run(init)
+    
+    # Training cycle
+    for epoch in range(training_epochs):
+        avg_cost = 0.
+        total_batch = int(2000/batch_size)
+        # Loop over all batches
+        for i in range(total_batch):
+            batch_xs = Xtr[i*100:(i*100)+100]
+            batch_ys = Ytr[i*100:(i*100)+100]
+            # Fit training using batch data
+            _, c = sess.run([optimizer, cost], feed_dict={x: batch_xs,
+                            y: batch_ys})
+                            # Compute average loss
+            avg_cost += c / total_batch
+        # Display logs per epoch step
+        if (epoch+1) % display_step == 0:
+            print "Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(avg_cost)
+
+    print "Optimization Finished!"
+    
+    # Test model
+    correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+    # Calculate accuracy for 3000 examples
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    print "Accuracy:", accuracy.eval({x: Xte, y: Yte})
